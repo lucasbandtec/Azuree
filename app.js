8fd0a3630b58d5e760b0efdaee68f0ab3a45d45f
Aluno <Aluno@Digital.net>
2018-10-24 20:14:29 -0200

    Algo

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var express = require('express');
 
-var http = require('http');
-http.createServer(function(req,res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Olá mundo!');
-}).listen(3000);
-console.log('Servidor iniciado em localhost:3000. Ctrl+C para encerrar…');var http = require('http'); http.createSe
+var app = express();
+
+
+const server = app.listen(3000);
+
+app.use('/',require('./controller/index'));
+
+console.log("Conectado");
